d6fc699c1454b63e89e479b5bc5a919a080a4853
Jothika-Selvaraj <jothikaselvaraj8787@gmail.com>
2024-11-07 14:51:48 +0530

    third

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // This tells Tailwind to scan your React files for classes
+    "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
